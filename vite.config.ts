af27aa0d912d9173470c829baed81b0b1ad45d67
FirelistiqGames <Firelistiq.games@gmail.com>
2025-05-12 21:45:55 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Maatschappij/",
+  base: "/partijvoorgezondheid/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
